622d1c1c7b446f2a7106bff1e9655f6819ddc780
Patrick Daniel Neary <pneary@umd.edu>
2020-06-14 12:54:44 -0400

    reverseLinkedList

---
+++ linkedLists/reverseLinkedList.java
@@ -0,0 +1,21 @@
+public ListNode reverseList(ListNode head) {
+        if (head == null){
+            return null;
+        }
+        
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        head = prev;
+        return head;
+        
+        
+    }
